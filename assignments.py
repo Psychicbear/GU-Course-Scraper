373121ac2fc31a41dbb52656c56eaf24188ca942
Riley <rjscho@live.com.au>
2021-11-16 06:49:10 +1000

    Overhauling classes again, currently added Submission class

--- assignments.py
+++ assignments.py
@@ -17,8 +17,8 @@ class Course():
             course_dict = {
                 'course_code': Usr_In.specific('Enter the course code:\nEg. 1701ICT\nCode: ','^\d{4}\w{3}$',err_msg='Syntax error, try again!'),
                 'name': input('Enter the name of the course: '),
-                'note': input('Enter the description/note for the course (leave blank for none): '),
-                'assessments': None
+                'description': input('Enter the description/note for the course (leave blank for none): '),
+                'assessments': {}
             }
         self.code = course_dict['course_code']
         self.name = course_dict['name']
@@ -31,7 +31,7 @@ class Course():
     
     #Adds assignment to course, creates a new assignment or loads existing if you pass down a dict and pos
     #Pos is used to order the assignments to make sure the order isn't different every time you view them like in a dict
-    def add_assignment(self,pos=len(self.assignments), assign_dict=None):
+    def add_assignment(self, pos=None, assign_dict=None):
         if assign_dict is None:
             assign_type = Usr_In.matching(
                 text['type_prompt'],
@@ -39,6 +39,8 @@ class Course():
                 usr_help=text['type_help'],
                 err_msg='Incorrect input, try again (tip: type is case sensitive)'
             )
+        if pos is None:
+            pos = len(self.assignments)
         else: assign_type = assign_dict['type']
         if assign_type == 'project':
             load = Project(pos, assign_dict)
@@ -48,7 +50,11 @@ class Course():
             pass
         self.assignments.append(load)
         #Re-sorts assignments into correct order
-        #How to into sort dict again? something like key=var.get() or something idk 
+        self.assignments.sort(key=lambda x:x.pos)
+
+    def info(self):
+        if self.note == '': self.note = 'No note given'
+        print(f'{self.code} {self.name} - {self.note}')
 
     #Exports the course to JSON format, bringing assignments with it
     def export(self):
@@ -98,12 +104,88 @@ class Project():
     
     #Prints some basic information about the contained data
     def info(self):
-        self.info = f'Assignment: {self.name}\nnote: {self.note}\nDue on: {self.due_date}\nMarked out of {self.max_mark}, it weighs {self.weight}\%\ of the course'
+        self.info = f'Assignment: {self.title}\nnote: {self.note}\nDue on: {self.due_date}\nMarked out of {self.max_mark}, it weighs {self.weight}\%\ of the course'
         print(self.info)
 
     #Exports the data into a dict for dumping into a JSON file
     def export(self):
-        return {self.pos: {'title': self.name, 'type':self.type, 'max_mark': self.max_mark,
+        return {self.pos: {'title': self.title,'note':self.note, 'type':'project', 'max_mark': self.max_mark,
         'est_mark': self.est_mark, 'act_mark': self.act_mark, 'weight': self.weight,
         'submitted': self.submitted, 'marked': self.marked, 
         'due':{'D': self.due_date.day, 'M': self.due_date.month, 'Y': self.due_date.year}}}
+
+class Workshop():
+    def __init__(self, assign_dict=None):
+        if assign_dict is None:
+            assign_dict = {
+                'title': input('What is the name of the assignment?: '),
+                'note': input('Give a description/note for assignment (blank to skip)'), 
+                'max_mark': Usr_In.number_int('What is the assignment marked out of? (number): ', err_msg='Expected whole number'),
+                'est_mark': 0, 
+                'act_mark': 0, 
+                'weight': Usr_In.specific(
+                    text['proj_weight_prompt'],
+                    regex['weight'],
+                    usr_help=text['proj_weight_help'],
+                    err_msg='Incorrect range, try again!'
+                ),
+                'submitted': False, 
+                'marked': False, 
+                'due':  {}
+            }
+            #Ask total weight of weeks
+            weight = Usr_In.number_int('What is the total weight of all weeks combined?\n ?: ', err_msg='Expected whole number') / 100
+            #Prompt how many weeks there are
+            total_weeks = Usr_In.number_int('How many weeks are assessed?\n ?: ')
+            #Ask which week assessed weeks start
+            start_week = Usr_In.number_int('What week do these assessments start?\n ?: ')
+            #Ask how many weeks have different value
+            diff_weeks = []
+            diff_weight = 0.0
+            amt_diff_weeks = input('If any weeks are worth more, how many? (leave blank if none)\n ?: ')
+            #Loop and ask for week and value
+            if amt_diff_weeks != '':
+                total = int(amt_diff_weeks)
+                for i in range(total):
+                    print(f'{i}/{total} different weeks')
+                    current_week = Usr_In.number_int('Week number of different weight?\n ?: ')
+                    current_weight = Usr_In.number_int('What is the weight of this week?\n ?: ', err_msg='Expected whole number') / 100
+                    diff_weight += current_weight
+                    diff_weeks.append([current_week,current_weight])
+                reg_weight = (weight - diff_weight)/(total_weeks - amt_diff_weeks)
+            else: reg_weight = weight/total_weeks
+            #Generate each week
+            weeks = {}
+            for i in range(total_weeks):
+                week_num = i+start_week
+                if diff_weeks != []:
+                    for j in diff_weeks:
+                        if week_num in j: week_weight = j[1]
+                        else: week_weight = reg_weight
+                else: week_weight = reg_weight
+                week_dict = {
+                    'week': week_num
+                }
+
+#Submission class
+#Is created for every instance where a mark and due date is used eg. Assignment, Workshop etc
+#Takes a parent and optional existing data, substituting the existing data into the instance when provided
+class Submission():
+    def __init__(self, parent, existingData=False):
+        self.parent = parent #The class in which this submission is contained within
+        self.data = existingData #Existing object containing data, will push contained data to attributes
+        self.dueDate = self.data['date'] if self.data else date #datetime format
+        self.isSubmitted = self.data['submitted'] if self.data else False #Boolean to be changed once assignment submitted
+        self.isMarked = self.data['submitted'] if self.data else False #Boolean to be changed once assignment marked
+        self.totalMarks = self.data['total'] if self.data else Usr_In.number_int('Please enter the total marks of this item', err_msg = "Incorrect input, please ensure you're entering a whole number") #Int
+        self.guessMarks = self.data['guess'] if self.data else 0 #Int for predicted marks
+        self.actualMarks = self.data['actual'] if self.data else 0 #Int for actual marks when recieved
+        self.weight = self.data['weight'] if self.data else Usr_In.number_int('Please enter weight of this item (whole number without percentage)') / 100 #2f float, percentage value of mark
+        self.calculatedMark = self.data['calculated'] if self.data else 0.00 #Calculated total value of submission in relation to full class mark 
+
+    def calculateMarks(self):
+        pass
+        #self.calculatedPercentage =  (self.guessMarks/self.totalMarks) if self.guessMarks
+
+
+            
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import errorHandling as Usr_In, json
-from assignments import NewCourse, NewProject
+import errorHandling as Usr_In, json, re
+from assignments import Course, Project
 dataJSON = {}
 loadedCourses = []
+user = ''
 def saveJSON():
+    global dataJSON
     for i in loadedCourses:
-        global dataJSON
         dataJSON.update(i.export())
     with open('data.json',mode='w') as outfile:
         json.dump(dataJSON, outfile, indent=4)
@@ -13,43 +14,75 @@ def saveJSON():
 def loadJSON():
     with open('data.json',mode='r') as infile:
         data = json.load(infile)
-        global dataJSON, loadedCourses
+        global dataJSON, loadedCourses, user
         dataJSON.update(data)
         loadedCourses = []
-        for key in loadedCourses:
-            course = loadedCourses[key]
-            
-        return
+        for key in dataJSON:
+            if key == 'user':
+                user = dataJSON[key]
+                continue
+            course = Course(dataJSON[key])
+            loadedCourses.append(course) 
 
 def main():
+    global dataJSON, loadedCourses
     try:
-        data = open('data.json', 'r')
-        data.close()
+        loadJSON()
     except: 
         with open('data.json', 'w') as outfile:
             json.dump({'user': input('Please enter your name: ')}, outfile, indent=4)
-    loadJSON()
-    print('\n\n\nWelcome ' + dataJSON['user'])#Welcomes the loaded user
+        loadJSON()
+    print('\n\n\nWelcome ' + user)#Welcomes the loaded user
     print('Enter help for command list, or help [command] for an explanation of this command')
     
-    usr_in = 0
-    while usr_in != 'q':
-        usr_in = input('Enter Command: ')
+    while True:
+        usr_in = input('?: ')
+        split_input = usr_in.strip().split()
+        if usr_in == 'save':
+            saveJSON()
+        elif usr_in == 'load':
+            loadJSON()
+        elif usr_in == 'add course':
+            load = Course()
+            loadedCourses.append(load)
+        elif re.search('^(view) \d{4}\w{3}$', usr_in):
+            course = split_input[1]
+            found = False
+            for i in loadedCourses:
+                if i.code == course:
+                    course = i
+                    found = True
+                    break
+            if found is True:
+                view_course(course)
+            else: print('Course not found')
+        elif usr_in == 'quit':
+            break
+        else: print('Invalid command')
     print('Thank you for using my software!')
 
-def add_course():
-    print('Add a course to list')
-    course_code = input('Enter Course Code: ')
-    name = input('What is the name of the course?: ')
-    note = input('Would you like to add a note/description of course? (leave empty if none)')
-
-
-def REPL():
+def view_course(course):
+    #Show total amount of assignments
+    #Show completed assignments
+    #Show if weekly assignment submitted
+    #Show current estimated grade
     while True:
-        usr_in = input('Enter Command: ')
+        total_assign = 0
+        complete_assign = 0
+        course.info()
+        for i in course.assignments:
+            i.info()
+            total_assign += 1
+            if i.submitted is True:
+                complete_assign += 1
+        print(f'Completed assignments: {complete_assign}/{total_assign}')
+        usr_in = input('Enter command or type back to go back: ')
+        if re.search('^(view) \d$', usr_in):
+            index = int(''.join(usr_in[5:]))
+            view_assignment(course.assignment[index])
 
+def view_assignment(assign):
+    pass
 
 #main()
-loadedCourses.append(NewCourse())
-loadedCourses.append(NewCourse())
-saveJSON()
+print(re.search('(\d) (\d\d)', '2 54 64 234 1 2'))
